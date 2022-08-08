35157096e102d8c7070e143ed1f5d37f5381aa88
rhuaannn <rhuanmarques1223@gmail.com>
2022-08-07 22:37:13 -0300

    updat readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Testando o github. (=
+
+## Update.
---
+++ code.js
@@ -0,0 +1,10 @@
+let valor = prompt ("informe um numero inteiro")
+let valor1= prompt ("informe o segundo numero inteiro")
+let resultado = valor*valor1
+
+if(resultado <50){
+alert("Seu resultado foi divido por 2 :"+resultado/2)
+
+}else {
+    alert("seu resultado sem divisão é:" + resultado)
+}
