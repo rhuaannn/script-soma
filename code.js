6a377cea57ff6810495ee41e53456301724cd069
rhuaannn <rhuanmarques1223@gmail.com>
2022-08-24 21:08:05 -0300

    commit

--- code.js
+++ code.js
@@ -9,43 +9,50 @@ alert("Seu resultado foi divido por 2 :"+resultado/2)
     alert("seu resultado sem divisão é:" + resultado)
 }
 */
-
+window.onload = function()
+{
+    document.getElementById("n1").addEventListener('keypress', function(event)
+    {
+        if (!Math.sign(event.key) &&  (event.key !== "Backspace")) 
+        {
+            event.preventDefault();
+        }
+    });
+    document.getElementById("n2").addEventListener('keypress', function(event)
+    {
+        if (!Math.sign(event.key) &&  (event.key !== "Backspace")) 
+        {
+            event.preventDefault();
+        }
+    });
+}
 
 
 function multiplicar() {
+    
     var n1 = document.querySelector('#n1').value
     var n2 = document.querySelector('#n2').value
     var resultado = document.querySelector('#resultado')
 
-    resultado.innerHTML = parseInt(n1) * parseInt(n2)
+    if(n1 ==="" || n2===""){
+        alert("Favor digitar um número")
+    }else{
+        resultado.innerHTML = parseInt(n1) * parseInt(n2)
+    }
+  
 
-}
-window.onload = function()
-{
-    document.getElementById('n1').addEventListener('keydown', function(event)
-    {
-        if (!Math.sign(event.key)&& (event.key!=="Backspace"))
-        {
-            event.preventDefault();
-        }
-    });
-}
-window.onload = function()
-{
-    document.getElementById('n2').addEventListener('keydown', function(event)
-    {
-        if (!Math.sign(event.key)&& (event.key!=="Backspace"))
-        {
-            event.preventDefault();
-        }
-    });
-}
+}    
+
 function somar() {
     var n1 = document.querySelector('#n1').value
     var n2 = document.querySelector('#n2').value
     var resultado = document.querySelector('#resultado')
 
-    resultado.innerHTML = parseInt(n1) + parseInt(n2)
+    if(n1 ==="" || n2===""){
+        alert("Favor digitar um número válido")
+    }else{
+        resultado.innerHTML = parseInt(n1) + parseInt(n2)
+    }
 
 }
 function subtrair() {
@@ -53,14 +60,20 @@ function subtrair() {
     var n2 = document.querySelector('#n2').value
     var resultado = document.querySelector('#resultado')
 
-    resultado.innerHTML = parseInt(n1) + parseInt(n2)
-
+    if(n1 ==="" || n2===""){
+        alert("Favor digitar um número válido")
+    }else{
+        resultado.innerHTML = parseInt(n1) - parseInt(n2)
+    }
 }
 function divisao() {
     var n1 = document.querySelector('#n1').value
     var n2 = document.querySelector('#n2').value
     var resultado = document.querySelector('#resultado')
 
-    resultado.innerHTML = parseInt(n1) / parseInt(n2)
-
+    if(n1 ==="" || n2===""){
+        alert("Favor digitar um número")
+    }else{
+        resultado.innerHTML = parseInt(n1) / parseInt(n2)
+    }
 }
